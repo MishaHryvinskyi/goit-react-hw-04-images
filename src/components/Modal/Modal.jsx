33e47dfd2b3c2ce02f0c9b5859fd77e4702eae27
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-10 23:30:50 +0300

    Modal => hooks, Searchbar => hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
+import React,{ useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalDiv } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscapeClick);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscapeClick);
-  }
-
-  handleEscapeClick = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropClick = e => {
+const Modal =({ onClose, children }) => {
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
-  };
+  };  
 
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalDiv>{children}</ModalDiv>
-      </Overlay>,
-      modalRoot
-    );
-  }
+  useEffect(() => {
+    const handleEscapeClick = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    
+    window.addEventListener('keydown', handleEscapeClick);
+    return () => {
+      window.removeEventListener('keydown', handleEscapeClick);
+    }
+
+  }, [onClose])
+  
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalDiv>{children}</ModalDiv>
+    </Overlay>,
+    modalRoot
+  );
 }
 
 Modal.propTypes = {
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,54 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Search, SearchForm, SearchFormBtn, SearchInput } from './Searchbar.styled';
 import { FcSearch } from 'react-icons/fc';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class Searchbar extends Component {
-  state = {
-    search: '',
+const Searchbar = ({ onSubmit }) => {
+  const [search, setSearch] = useState('');
+  
+  const handleSearchChange = e => {
+    setSearch(e.currentTarget.value.toLowerCase());
   };
 
-  handleSearchChange = e => {
-    this.setState({ search: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.search.trim() === '') {
+    if (search.trim() === '') {
       toast.error('Please enter a search query');
       return;
     }
 
-    this.props.onSubmit(this.state.search);
-    this.setState({ search: '' });
+    onSubmit(search);
+    setSearch('');
   };
 
-  render() {
-    const { search } = this.state;
-    return (
-      <Search>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormBtn type="submit">
-            <FcSearch size="24" />
-          </SearchFormBtn>
+  return (
+    <Search>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormBtn type="submit">
+          <FcSearch size="24" />
+        </SearchFormBtn>
 
-          <SearchInput
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={search}
-            onChange={this.handleSearchChange}
-          />
-        </SearchForm>
-        <ToastContainer />
-      </Search>
-    );
-  }
-}
+        <SearchInput
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={search}
+          onChange={handleSearchChange}
+        />
+      </SearchForm>
+      <ToastContainer />
+    </Search>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
