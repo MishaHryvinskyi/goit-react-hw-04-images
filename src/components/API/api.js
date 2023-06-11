2d60155dc4b9cd505142f51490d34bda4d642871
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-11 17:15:37 +0300

    add axios

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -1,24 +1,38 @@
+import axios from 'axios';
+
 const fetchImages = (search, page, images) => {
-    return fetch(
-      `https://pixabay.com/api/?q=${search}&page=${page}&key=35643945-433c06e40cd86730ec72beccd&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(res => res.json())
-      .then(data => {
-        const newImages = [...images, ...data.hits];
-        const hasMoreImages = data.hits.length >= 12;
-  
-        return {
-          images: newImages,
-          loading: false,
-          showLoadMoreButton: hasMoreImages,
-        };
-      })
-      .catch(error => {
-        console.log('Error fetching data:', error);
-        return {
-          loading: false,
-        };
-      });
-  };
-  
-  export default fetchImages;
+  const API_URL = 'https://pixabay.com/api/';
+  const API_KEY = '35643945-433c06e40cd86730ec72beccd';
+  const perPage = 12;
+
+  return axios
+    .get(API_URL, {
+      params: {
+        key: API_KEY,
+        q: search,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: perPage,
+        page,
+      },
+    })
+    .then(response => {
+      const { data } = response;
+      const newImages = [...images, ...data.hits];
+      const hasMoreImages = data.hits.length >= perPage;
+
+      return {
+        images: newImages,
+        loading: false,
+        showLoadMoreButton: hasMoreImages,
+      };
+    })
+    .catch(error => {
+      console.log('Error fetching data:', error);
+      return {
+        loading: false,
+      };
+    });
+};
+
+export default fetchImages;
